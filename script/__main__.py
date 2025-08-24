79a0e6dad4f5ac115887962632b1c69face4cd1c
Louis Billaut <ragedfear4875@gmail.com>
2025-08-24 14:41:50 +0800

    add fading particle effect

--- script/__main__.py
+++ script/__main__.py
@@ -543,47 +543,59 @@ class Text:
 class Particle:
     def __init__(self,
                  shape: Shapes = Shapes.CIRCLE,
-                 color: tuple [int, int, int] = (0, 0, 0),
+                 color: list[int, int, int, int] = (0, 0, 0, 255),
                  pos: tuple[float, float] = (0.0, 0.0),
                  size: float = 1.0,
                  velocity: tuple[float, float] = (0.0, 0.0),
                  gravity: float = 1,
                  timer: int = 3,
-                 is_randomized: tuple[bool, bool] = (True, True)) -> None:
+                 is_randomized: tuple[bool, bool] = (True, True),
+                 is_decreasing_opacity: bool = True) -> None:
         
         self.shape = shape
         self.color = color
+        self.max_opacity = self.color[3]
         self.size = size
         self.x, self.y = pos
         self.x_velocity, self.y_velocity = velocity
         self.has_random_x, self.has_random_y = is_randomized
         self.gravity = gravity
         self.max_timer = self.timer = timer*C.FPS
+        self.decrease_opacity = is_decreasing_opacity
         
-        if self.has_random_x:
-            rv = random.random()
-            self.x_velocity *= (rv**2 + rv) * (-1)**random.randint(1, 2)
+        self.x_velocity = self.set_random_velocity(self.has_random_x, self.x_velocity)
+        self.y_velocity = self.set_random_velocity(self.has_random_y, self.y_velocity)
 
-        if self.has_random_y:
-            rv = random.random()
-            self.y_velocity *= (rv**2 + rv) * (-1)**random.randint(1, 2)
-    
-    def update_and_draw(self, screen: pg.Surface, particle_list: list):
+    def update_and_draw(self, screen: pg.Surface, particle_list: list) -> None:
+        self.timer -= 1
+        if self.timer <= 0:
+            particle_list.remove(self)
+            del self
+            return
+        
         self.x += self.x_velocity
         self.y += self.y_velocity + self.gravity
 
         self.x_velocity -= self.x_velocity/self.max_timer
         self.y_velocity -= self.y_velocity/self.max_timer
 
-        self.timer -= 1
-
-        if self.timer <= 0:
-            particle_list.remove(self)
-            del self
+        if self.decrease_opacity:
+            self.color[3] -= self.max_opacity/self.max_timer
         
         match self.shape:
             case Shapes.CIRCLE:
-                pg.draw.circle(screen, self.color, (self.x, self.y), self.size)
+                if not self.decrease_opacity:
+                    pg.draw.circle(screen, self.color, (self.x, self.y), self.size)
+                else:
+                    particle_surface = pg.Surface((self.size*2, self.size*2), pg.SRCALPHA)
+                    pg.draw.circle(particle_surface, self.color, (self.size, self.size), self.size)
+                    screen.blit(particle_surface, (self.x - self.size, self.y - self.size))
+
+    def set_random_velocity(self, condition: bool, value: float) -> float:
+        if condition:
+            rand_val = random.random()
+            value *= (rand_val**2 + rand_val) * (-1)**random.randint(1, 2)
+        return value
 
 button_1 = Button(image = pg.image.load(MENU_PATH + "Achievement.png").convert_alpha(), pos = (200, 200))
 mouse = Mouse()
@@ -624,7 +636,7 @@ while True:
     for particle in particles:
         particle: Particle
         particle.update_and_draw(screen = screen, particle_list = particles)
-    #particles.append(Particle(color = (255,128,255), pos = mouse.pos, size = 3, velocity = (1,1), gravity = 1))
+    particles.append(Particle(color = [255,128,255,255], pos = mouse.pos, size = 3, velocity = (1,1), gravity = 1))
 
     # Event Handling -------------------------------------------------------------------------------------
     for event in pg.event.get():
