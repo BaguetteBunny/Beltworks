0f31dc1d5ee8132dac4d85d4ad49e7965cef46df
Louis Billaut <ragedfear4875@gmail.com>
2025-09-14 15:08:15 +0800

    init recipe script

---
+++ script/recipe.py
@@ -0,0 +1,32 @@
+POWDER_RECIPE = {
+    "amber_powder":     [("raw_amber_ore", "raw_ore", 150), (), (), (), ()],
+    "amethyst_powder":  [("raw_amethyst_ore", "raw_ore", 150), (), (), (), ()],
+    "bronze_powder":    [("raw_bronze_ore", "raw_ore", 150), (), (), (), ()],
+    "emerald_powder":   [("raw_emerald_ore", "raw_ore", 150), (), (), (), ()],
+    "gold_powder":      [("raw_gold_ore", "raw_ore", 150), (), (), (), ()],
+    "onyx_powder":      [("raw_onyx_ore", "raw_ore", 150), (), (), (), ()],
+    "sapphire_powder":  [("raw_sapphire_ore", "raw_ore", 150), (), (), (), ()],
+    "silver_powder":    [("raw_silver_ore", "raw_ore", 150), (), (), (), ()],
+}
+
+GEMSTONE_RECIPE = {
+    "amber_gemstone":     [("amber_powder", "powder", 250), (), (), (), ()],
+    "amethyst_gemstone":  [("amethyst_powder", "powder", 250), (), (), (), ()],
+    "bronze_gemstone":    [("bronze_powder", "powder", 250), (), (), (), ()],
+    "emerald_gemstone":   [("emerald_powder", "powder", 250), (), (), (), ()],
+    "gold_gemstone":      [("gold_powder", "powder", 250), (), (), (), ()],
+    "onyx_gemstone":      [("onyx_powder", "powder", 250), (), (), (), ()],
+    "sapphire_gemstone":  [("sapphire_powder", "powder", 250), (), (), (), ()],
+    "silver_gemstone":    [("silver_powder", "powder", 250), (), (), (), ()],
+}
+
+REFINED_GEMSTONE_RECIPE = {
+    "refined_amber_gemstone":     [("amber_gemstone", "gemstone", 1000), (), (), (), ()],
+    "refined_amethyst_gemstone":  [("amethyst_gemstone", "gemstone", 1000), (), (), (), ()],
+    "refined_bronze_gemstone":    [("bronze_gemstone", "gemstone", 1000), (), (), (), ()],
+    "refined_emerald_gemstone":   [("emerald_gemstone", "gemstone", 1000), (), (), (), ()],
+    "refined_gold_gemstone":      [("gold_gemstone", "gemstone", 1000), (), (), (), ()],
+    "refined_onyx_gemstone":      [("onyx_gemstone", "gemstone", 1000), (), (), (), ()],
+    "refined_sapphire_gemstone":  [("sapphire_gemstone", "gemstone", 1000), (), (), (), ()],
+    "refined_silver_gemstone":    [("silver_gemstone", "gemstone", 1000), (), (), (), ()],
+}
