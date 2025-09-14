83d6235a5237675f71a5933e7e2ef7c1b8db43a5
Louis Billaut <ragedfear4875@gmail.com>
2025-09-14 18:13:44 +0800

    adjust recipe quantity

--- script/recipe.py
+++ script/recipe.py
@@ -1,12 +1,12 @@
 POWDER_RECIPE = {
     "amber_powder":     [("raw_amber_ore", "raw_ore", 100), (), (), (), ()],
     "amethyst_powder":  [("raw_amethyst_ore", "raw_ore", 75), (), (), (), ()],
-    "bronze_powder":    [("raw_bronze_ore", "raw_ore", 100), (), (), (), ()],
+    "bronze_powder":    [("raw_bronze_ore", "raw_ore", 150), (), (), (), ()],
     "emerald_powder":   [("raw_emerald_ore", "raw_ore", 100), (), (), (), ()],
     "gold_powder":      [("raw_gold_ore", "raw_ore", 100), (), (), (), ()],
     "onyx_powder":      [("raw_onyx_ore", "raw_ore", 75), (), (), (), ()],
     "sapphire_powder":  [("raw_sapphire_ore", "raw_ore", 100), (), (), (), ()],
-    "silver_powder":    [("raw_silver_ore", "raw_ore", 200), (), (), (), ()],
+    "silver_powder":    [("raw_silver_ore", "raw_ore", 150), (), (), (), ()],
 }
 
 GEMSTONE_RECIPE = {
@@ -23,7 +23,7 @@ GEMSTONE_RECIPE = {
 REFINED_GEMSTONE_RECIPE = {
     "refined_amber_gemstone":     [("amber_gemstone", "gemstone", 1000), (), (), (), ()],
     "refined_amethyst_gemstone":  [("amethyst_gemstone", "gemstone", 750), (), (), (), ()],
-    "refined_bronze_gemstone":    [("bronze_gemstone", "gemstone", 1000), (), (), (), ()],
+    "refined_bronze_gemstone":    [("bronze_gemstone", "gemstone", 10_000), (), (), (), ()],
     "refined_emerald_gemstone":   [("emerald_gemstone", "gemstone", 1000), (), (), (), ()],
     "refined_gold_gemstone":      [("gold_gemstone", "gemstone", 1000), (), (), (), ()],
     "refined_onyx_gemstone":      [("onyx_gemstone", "gemstone", 750), (), (), (), ()],
